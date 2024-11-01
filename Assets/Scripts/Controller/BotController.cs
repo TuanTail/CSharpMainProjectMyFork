0abd189e3e16423673e9ee4c5c9c04742fff1e03
TuanTail <tuan11@mail.ru>
2024-11-01 14:54:11 +0300

    fedor E

--- Assets/Scripts/Controller/BotController.cs
+++ Assets/Scripts/Controller/BotController.cs
@@ -6,9 +6,7 @@ using Model;
 using Model.Config;
 using UnityEngine;
 using Utilities;
-//hello i'm fine, and u?
-//me too, ty!
-// xyz
+//hello i'm fedor
 namespace Controller
 {
     public class BotController
